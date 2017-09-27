9b4be7c306a37dc2552cc383ac37d6efac3904d9
nzinferno <devils_cowboy@hotmail.com>
2017-09-27 17:16:25 +1300

    Add files via upload

---
+++ boolenaExercises.js
@@ -0,0 +1,13 @@
+if (age < 0) {
+	console.log("Error!");
+}
+
+if (age === 21) {
+console.log("Happy 2st Birthday!");
+}
+
+If (age % 2 === 1) {
+	console.log("your age is odd!");
+}
+
+
---
+++ whileLoopProblemSet.js
@@ -0,0 +1,20 @@
+var num = -10
+
+while (num>-11 && num<20) {
+	console.log(num);
+	num++;
+}
+
+var num = 10
+
+while (num<=40) {
+	console.log(num);
+	num+=2;
+}
+
+var num = 300
+
+while (num<) {
+	console.log(num);
+	num+=2;
+}
