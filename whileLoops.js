b7b627d4241ffbfd37d79554bff39859e7ee6f28
nzinferno <devils_cowboy@hotmail.com>
2017-10-04 15:41:49 +1300

    Add files via upload

--- whileLoops.js
+++ whileLoops.js
@@ -16,10 +16,8 @@ console.log(" Printing all numbers divisible by 5 and 3 between 5 and 50")
 var number = 5
 
 while(number <=50) {
-	if (number % 3 === 0) {
+	if (number % 3 === 0  && number % 5 === 0) {
 	console.log(number);
-   } else if (number % 5 === 0) {
-   	console.log(number);
    }
    number++;
 }
