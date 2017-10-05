fde92381955513e99422bf25e52d804a2b7b7798
nzinferno <devils_cowboy@hotmail.com>
2017-10-05 19:11:45 +1300

    Add files via upload

---
+++ forLoops.js
@@ -0,0 +1,29 @@
+console.log("Printing all numbers between -10 and 19!")
+
+for (var i = -10 ; i <= 19; i++) {
+	console.log(i);
+}
+
+console.log("Printing all EVEN numbers between 10 and 40!")
+
+for (var i = 10 ; i <= 40; i++) {
+	if(i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+console.log("Printing all ODD numbers between 300 and 333!")
+
+for (var i = 300; i <= 333; i++) {
+	if(i % 2 !== 0) {
+		console.log(i);
+	}
+}
+
+console.log("Printing all numbers divisible by 5 and 3 between 5 and 50!")
+
+for (var i = 5; i <= 50; i++) {
+	if(i % 3 === 0 && i % 5 === 0) {
+		console.log(i);
+	}
+}
