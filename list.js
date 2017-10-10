978891f541815b582dd6ebc0de91cb5c19025824
nzinferno <devils_cowboy@hotmail.com>
2017-10-10 15:10:08 +1300

    Add files via upload

--- list.js
+++ list.js
@@ -1,2 +1,19 @@
-var answer = prompt("What would you like to do?");
+var todos = ["Buy new Turtle"];
 
+var input = prompt("What would you like to do?");
+
+while (input !== "quit") {
+	// handle input
+  if (input === "list") {
+	console.log(todos);
+  } else if (input === "new") {
+	// ask for new todo
+    var newTodo = prompt("Enter new Todo");
+	// add to array
+    todos.push(newTodo);
+  }
+
+// ask again for new input
+  input = prompt("What would you like to do?");
+}
+console.log("Ok, you quit the app");
