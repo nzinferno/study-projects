e0ba56dcbc3a8f10599eef9a76dd11fa698dbff2
nzinferno <devils_cowboy@hotmail.com>
2017-10-19 16:55:21 +1300

    Add files via upload

---
+++ domExercise.js
@@ -0,0 +1,5 @@
+var p1 = document.getElementsByClassName("special") [0];
+var p2 = document.getElementById("first");
+var p3 = document.querySelector("#first");
+var p4 = document.querySelector("p");
+var p5 = document.getElementsByTagName("p") [0];
