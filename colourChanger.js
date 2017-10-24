495b6383bdfc22eca6bbd0934514c391f286c27f
nzinferno <devils_cowboy@hotmail.com>
2017-10-25 12:13:50 +1300

    Add files via upload

--- colourChanger.js
+++ colourChanger.js
@@ -1,9 +1,16 @@
 var button = document.querySelector("button");
 
-button.addEventListener("click", function () {
-		if(document.body.style.background === "white") {
-		document.body.style.background = "purple";
-	} else {
-		document.body.style.background = "white";
-	}
-})
+// button.addEventListener("click", function () {
+// 	// if White
+// 	  // Make it purple
+//     // else make it white
+// 		if(document.body.style.background === "white") {
+// 		document.body.style.background = "purple";
+// 	} else {
+// 		document.body.style.background = "white";
+// 	}
+// });
+
+button.addEventListener("click", function(){
+  document.body.classList.toggle("purple");
+});
