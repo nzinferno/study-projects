fa4a8c05ce6566d76b06864cc5782e1bf31d7bdc
nzinferno <devils_cowboy@hotmail.com>
2017-11-01 19:55:53 +1300

    Add files via upload

--- rgbColourPickerGame.js
+++ rgbColourPickerGame.js
@@ -1,4 +1,4 @@
-var colours = [
+var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 255, 0)",
   "rgb(0, 255, 0)",
@@ -7,9 +7,23 @@ var colours = [
   "rgb(255, 0, 255)",
  ]
 
- var squares = document.querySelectorAll(".square");
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
 
- for (var i = 0; i < squares.length; i++) {
+for (var i = 0; i < squares.length; i++) {
+	// add initial colours to squares
  	squares[i].style.backgroundColor = colors[i];
-
- }
+ 	// add click listneres to squares
+ 	squares[i].addEventListener("click", function() {
+      // grab colour of clicked square
+      var clickedColor = this.style.backgroundColor;
+      // compare colour to pickedColor
+      if(clickedColor === pickedColor) {
+      	alert("correct");
+      } else {
+      	alert("Wrong!");
+      }
+ 	});
+}
