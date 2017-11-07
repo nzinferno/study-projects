b4cf74e62205122537560621c16f43ecc0079dfe
nzinferno <devils_cowboy@hotmail.com>
2017-11-07 17:08:39 +1300

    Add files via upload

---
+++ jQueryExercise.js
@@ -0,0 +1,8 @@
+// Select all divs, give them a purple background
+$("div").css("background", "purple");
+// Select divs with class highlight, make them 200px wide
+$(".highlight").css("width", "200px");
+// Select the Div with id "third" and give it a orange border
+$("#third").css("border", "2px solid orange");
+// Select the first div, change its font colour to pink
+$("div:first-of-type").css("color", "pink");
