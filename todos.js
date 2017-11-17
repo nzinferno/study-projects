e0a60e634e734138755a867b37ac2926403a87ac
nzinferno <devils_cowboy@hotmail.com>
2017-11-17 15:20:34 +1300

    Add files via upload

--- todos.js
+++ todos.js
@@ -3,3 +3,21 @@
 $("li").click(function(){
   $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("span").click(function(event) {
+  $(this).parent().fadeOut(500, function(){
+  	$(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+  	// grabbing new todo text from input
+  	var todoText = $(this).val();
+  	$(this).val("");
+  	// create a new li and add to ul
+  	$("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
+});
